93a261f93fcd92715cef63b9694a371db679f233
Edson Barros Ponciúncula <barrosedsonp@gmail.com>
2025-11-02 14:20:37 -0300

    Implementação da página e banco de dados de acervos

--- app_biblioteca/models.py
+++ app_biblioteca/models.py
@@ -4,7 +4,7 @@ def salvarDados(dados_atualizados):
     with open('app_biblioteca/registros.json', 'w') as arquivo:
         json.dump(dados_atualizados, arquivo, indent=4, ensure_ascii=False)
 
-def extrairDados():
-    with open('app_biblioteca/registros.json', 'r') as arquivo:
+def extrairDados(base_dados):
+    with open(f'app_biblioteca/{base_dados}.json', 'r') as arquivo:
         dados = json.load(arquivo)
     return dados
